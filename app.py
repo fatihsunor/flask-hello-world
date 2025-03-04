24f342a795c26a2d3409b77f830c55ef77feaacf
Fatih Sunor <fatih.sunor@gmail.com>
2025-03-04 14:10:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Yo!'
+    # Get the value of the 'name' parameter from the URL
+    restaurant = request.args.get('restaurant')
+    menu_item = request.args.get('menu_item')
+    time = request.args.get('time')
+     
+    # Greet Hello to name provided in the URL Parameter
+    return "Your order is placed!\nRestaurant:".format(restaurant)
