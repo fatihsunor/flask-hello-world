88fca9f6afffb2f29be0da9334966a508e38473d
Fatih Sunor <fatih.sunor@gmail.com>
2025-03-05 10:34:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello_world():
     time = request.args.get('time')
     
     d = { 'restaurant': restaurant, 'menu_item': menu_item, 'time': time }
-    s = "Your order is placed! \n Restaurant: $restaurant"
-    s.format(**d)
+    respond = "Your order is placed! \n Restaurant: $restaurant"
+    respond.format(**d)
     return respond
