0eca80579287c9fcb5aee9d27defa7ff3e5bff3a
Fatih Sunor <fatih.sunor@gmail.com>
2025-03-05 10:03:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def hello_world():
     time = request.args.get('time')
      
     # Greet Hello to name provided in the URL Parameter
-    respond = '''Your order is placed!
-    Restaurant: {restaurant}
-    Food: {menu_item}
-    Time: {time}''',format(restaurant=restaurant, menu_item=menu_item, time = time)
+    respond = 'Your order is placed!' + 
+            'Restaurant:' + restaurant
+            'Food:' + menu_item
+            'Time:' + time
     return respond
