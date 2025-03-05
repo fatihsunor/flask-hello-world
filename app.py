49e84c6a887c106b7ac6718909ac9cbfb2c1f424
Fatih Sunor <fatih.sunor@gmail.com>
2025-03-05 11:04:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # Get the value of the 'name' parameter from the URL
+    # Get the value of the parameter from the URL
     restaurant = request.args.get('restaurant')
     menu_item = request.args.get('menu_item')
     time = request.args.get('time')
     
-    respond = "Your order is placed! \\n Restaurant:" + " " + restaurant + " " + "\\n Food:" + " " + menu_item
+    respond = "Your order is placed! \n Restaurant:" + " " + str(restaurant) + " " + "\n Food:" + " " + str(menu_item)
     return respond
